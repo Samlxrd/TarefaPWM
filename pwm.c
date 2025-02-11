80fa76c3774138db25460128da528ed79c8b113d
Samlxrd <samlord.dzn@gmail.com>
2025-02-11 13:26:42 -0300

    Implementada função para definir o duty cycle + movimentação da flange do servomotor para as posições de 180º, 90º e 0º

--- pwm.c
+++ pwm.c
@@ -8,14 +8,24 @@ const float PWM_DIVISER = 250.0; // divisor do clock para o PWM
 
 // Protótipo das funções
 void pwm_setup();
+void set_pwm_dc(uint16_t active_cycle);
 
 int main()
 {
     stdio_init_all(); //inicializa o sistema padrão de I/O
     pwm_setup(); //configura o PWM
 
-    while (true) {
+    set_pwm_dc(2400); // Ciclo ativo de 2.400 µs (180 graus)
+    sleep_ms(5000);
 
+    set_pwm_dc(1470); // Ciclo ativo de 1.470 µs (90 graus)
+    sleep_ms(5000);
+    
+    set_pwm_dc(500); // Ciclo ativo de 500 µs (0 graus)
+    sleep_ms(5000);
+
+    while (true) {
+        
     }
 }
 
@@ -28,4 +38,9 @@ void pwm_setup()
     pwm_set_clkdiv(slice, PWM_DIVISER); //define o divisor de clock do PWM
     pwm_set_wrap(slice, WRAP_PERIOD); //definir o valor de wrap
     pwm_set_enabled(slice, true); //habilita o pwm no slice correspondente
+}
+
+void set_pwm_dc(uint16_t active_cycle) {
+    uint16_t duty_cycle = (active_cycle * WRAP_PERIOD) / 20000;
+    pwm_set_gpio_level(PWM_SERVO, duty_cycle);
 }
